20cda3cac8872173265287407fbf87f45a52336a
Faruk OZT <asimfaruk.ozturk@vodafone.com>
2023-10-22 23:41:26 +0300

    remove log

--- api-gateway/src/main/java/com/banka/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/banka/apigateway/config/GatewayConfig.java
@@ -9,19 +9,18 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 @Configuration
 public class GatewayConfig {
 
-    @Autowired
-    private AuthenticationFilter filter;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r
-                        .path("/auth-service/**").filters(f -> f.filter(filter))
+                        .path("/auth-service/**")
                         .uri("http://localhost:8081"))
                 .route("invoice", r -> r
-                        .path("/invoice/**").filters(f -> f.filter(filter))
+                        .path("/bill-payment-service/**")
                         .uri("http://localhost:8082"))
                 .route("mail-sender", r -> r
-                        .path("/mail-sender/**").filters(f -> f.filter(filter))
+                        .path("/mail-sender/**")
                         .uri("http://localhost:8083"))
                 .build();
     }
